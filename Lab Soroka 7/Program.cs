df5813ceb65685225f5c157f0f6e185533f39c8d
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-11-06 18:29:04 +0200

    Добавьте файлы проекта.

---
+++ Lab Soroka 7/Program.cs
@@ -0,0 +1,21 @@
+﻿//Вариант 24
+//Random randomizer = new Random();
+//string[] array = new string[5];
+
+//for (var i = 0; i < array.Length; i++)
+//{
+//    array[i] = Convert.ToString(randomizer.Next( 0, 11 ),2);
+//}
+
+//Console.Write("Начальный массив: ");
+//Console.WriteLine(string.Join(" , ", array));
+
+//string temp = array[array.Length - 1];
+//for (int i = array.Length - 1; i > 0; i--)
+//{
+//    array[i] = array[i - 1];
+//}
+//array[0] = temp;
+
+//Console.Write("Результирующий массив: ");
+//Console.WriteLine(string.Join(", ", array));
